452e5a62325a46b6b091dca76484da9c7d04b0f5
gopalshankar <gopalshankar@gmail.com>
2011-10-20 14:24:41 +0530

    Update msgque_const.h

--- msgque_const.h
+++ msgque_const.h
@@ -14,8 +14,14 @@
 
 /* OTHERS */
 #define MQ_FREE 0xFFFF
-#define MQ_SUCCESS 0
 #define MQ_FAILED -1
+#define MQ_SUCCESS 0
+#define MQ_USER_BLOCKED 1
+#define MQ_USER_ACTIVE 2
+#define MQ_SENDER 3
+#define MQ_RECIEVER 4
+
+
 
 /* Error codes */
 
@@ -23,3 +29,4 @@
 #define MQ_ERR_MAX_MESSAGES -3
 #define MQ_ERR_MAX_RECIEVERS -4
 #define ERR_INVALID_MQ -5
+#define ERR_MQ_INUSE -6
